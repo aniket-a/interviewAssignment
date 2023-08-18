2b608168fe91791c8bf13b63cf665c1b0db843aa
aniket-gupta-aot <aniket.gupta@algoocean.com>
2023-08-18 23:36:02 +0530

    removed useState for build fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "../style/home.css"
 import { LuCalendarClock } from "react-icons/lu"
 import { AiFillRightCircle } from "react-icons/ai"
